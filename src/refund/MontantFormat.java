b89a3f0d2062ef775438e1fd8a189175d6729737
jeansalama <jeansos3@hotmail.com>
2017-02-26 00:17:10 -0500

    Ajout de la classe MontantFormat

---
+++ src/refund/MontantFormat.java
@@ -0,0 +1,72 @@
+package refund;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.ParseException;
+
+/**
+ * Classe qui gere les formats des montants d'assurances
+ *
+ * @author Jean Salama
+ */
+public class MontantFormat {
+
+    public static void main(String[] args) {
+        
+        try{
+            System.out.println(analyserReclamation("1,23$") + " (1,23$)");
+            System.out.println(analyserReclamation("1.23$") + " (1.23$)");
+            
+            System.out.println(analyserReclamation("1000.23$") + " (1000.23$)");            
+            System.out.println(analyserReclamation("1000,23$") + " (1000,23$)");
+            
+            System.out.println(formatRemboursement(analyserReclamation("1000,23$")) 
+                    + " (1000,23$)");
+            
+            System.out.println(analyserReclamation("1.2.3$") + " (erreur ?)");
+        } catch(ReclamationException re){
+            System.out.println(re.getLocalizedMessage());
+        }
+    }
+    
+    public static String formatRemboursement(double montant) {
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setDecimalSeparator('.');
+
+        DecimalFormat df = new DecimalFormat("0.00$", symbols);
+
+        return df.format(montant);
+    }
+
+    public static double analyserReclamation(String montantReclamation)
+            throws ReclamationException {
+
+        double montant;
+        DecimalFormat analyseur = obtenirBonAnalyseur(montantReclamation);
+
+        try {
+            montant = analyseur.parse(montantReclamation).doubleValue();
+        } catch (ParseException pe) {
+            throw new ReclamationException("Le format du montant de reclamation"
+                    + " est incorrect. Il doit être de la forme"
+                    + " 0.00$ ou 0,00$");
+        }
+
+        return montant;
+    }
+
+    private static DecimalFormat obtenirBonAnalyseur(String montantReclamation) {
+        
+        DecimalFormatSymbols symboles = new DecimalFormatSymbols();
+        
+        symboles.setDecimalSeparator('.');
+        DecimalFormat analyseur = new DecimalFormat("0.00$", symboles);
+        
+        if (montantReclamation.contains(",")) {
+            symboles.setDecimalSeparator(',');
+            analyseur = new DecimalFormat("0.00$", symboles);
+        }
+        return analyseur;
+    }
+}
