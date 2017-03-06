be6d543bc26318725d885190a35e6807f2671c80
maximegagnon <maxime_gagnon4@msn.com>
2017-03-06 08:52:04 -0500

    Retrait de méthodes privées maintenant gérée dans MontantFormat.

--- src/refund/Reclamation.java
+++ src/refund/Reclamation.java
@@ -3,6 +3,7 @@ package refund;
 /**
  * Classe visant à créer et gérer des objets Reclamation.
  *
+ * @author Maxime Gagnon
  */
 public class Reclamation {
 
@@ -21,6 +22,7 @@ public class Reclamation {
      */
     public Reclamation(int soin, Date date, String montantReclamation)
             throws ReclamationException {
+
         setSoin(soin);
         if (!date.contientUnJour()) {
             throw new ReclamationException();
@@ -46,6 +48,7 @@ public class Reclamation {
     }
 
     public void setSoin(int soin) throws ReclamationException {
+
         if (!validerSoin(soin)) {
             throw new ReclamationException("numéro de soin non valide.");
         }
@@ -58,18 +61,17 @@ public class Reclamation {
 
     public void setMontantReclamation(String montantReclam)
             throws ReclamationException {
+
         int longueur = montantReclam.length();
-        int indicePoint = montantReclam.indexOf(".");
         this.montantReclamationString = montantReclam;
         contientDevise(montantReclam, longueur);
-        validerSeparateur(indicePoint, montantReclam);
         verifierSiNegatif(montantReclam);
-        verifierSiDeuxDecimales(montantReclam, indicePoint);
         setMontantReclamationDouble(montantReclam);
     }
 
     private void setMontantReclamationDouble(String montantReclam)
             throws ReclamationException {
+
         try {
             this.montantReclamationDouble
                     = Double.parseDouble(montantReclam.substring(0,
@@ -82,42 +84,14 @@ public class Reclamation {
 
     /**
      * @param montantReclam Un nombre décimal représentant un montant.
-     * @param indicePoint L'indice du montant où se trouve le séparateur.
-     * @throws ReclamationException Si le montant n'a pas de séparateur ou si le
-     * montant à plus ou moins de deux décimales.
-     */
-    private void verifierSiDeuxDecimales(String montantReclam, int indicePoint)
-            throws ReclamationException {
-        if (!montantReclam.contains(".") || indicePoint == 0
-                || montantReclam.substring(indicePoint + 1).length() != 3) {
-            throw new ReclamationException("Le montant doit etre represente"
-                    + " exactement sur deux decimales apres la \"virgule\""
-                    + " et au moins une decimale avant!");
-        }
-    }
-
-    /**
-     * @param montantReclam Un nombre décimal représentant un montant.
      * @throws ReclamationException si le montant passé en paramètre est négatif
      */
     private void verifierSiNegatif(String montantReclam)
             throws ReclamationException {
+
         if (montantReclam.contains("-")) {
             throw new ReclamationException("Le montant de la réclamation ne"
                     + " peut pas être négatif!");
-        }
-    }
-
-    /**
-     * @param indicePoint L'indice du montant où se trouve le séparateur.
-     * @param montantReclam Un nombre décimal représentant un montant.
-     * @throws ReclamationException
-     */
-    private void validerSeparateur(int indicePoint, String montantReclam)
-            throws ReclamationException {
-        if (indicePoint == -1 && montantReclam.contains(",")) {
-            throw new ReclamationException("Le separateur decimal doit etre"
-                    + " un point et non une virgule.");
         }
     }
 
@@ -129,6 +103,7 @@ public class Reclamation {
      */
     private void contientDevise(String montantReclam, int longueur)
             throws ReclamationException {
+
         if (montantReclam.charAt(longueur - 1) != '$') {
             throw new ReclamationException("La devise doit etre en '$' "
                     + "et placee apres le montant.");
