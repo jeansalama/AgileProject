5610d105dfaf66cfa06c01538840bf2b89d5b73d
veroniquelucignano <lucignano.veronique@courrier.uqam.ca>
2017-03-13 18:25:40 -0400

    Debut Classe Dollar

---
+++ src/refund/Dollar.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refund;
+
+/**
+ *
+ * @author Billy
+ */
+public class Dollar {
+
+    private final long cents;
+
+    public Dollar() {
+        cents = 0;
+    }
+
+    public Dollar(long cents){
+        
+        this.cents = cents;
+    }
+    
+    
+    
+}
