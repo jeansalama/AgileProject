50c776e4c4a8c4ff8bc053996c6a92b6b5aeccf4
veroniquelucignano <lucignano.veronique@courrier.uqam.ca>
2017-04-12 20:03:43 -0400

    Refactoring DateException sortie de la classe Date

---
+++ src/main/java/refund/DateException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refund;
+
+public class DateException extends Exception{
+      public DateException() {
+    }
+
+    public DateException(String msg) {
+        super(msg);
+    }
+}
