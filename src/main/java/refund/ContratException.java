a2dd40618eef07b00ff23e17883111c20e9e9ffd
veroniquelucignano <lucignano.veronique@courrier.uqam.ca>
2017-04-14 01:43:22 -0400

    Refactoring Classe ContratException

---
+++ src/main/java/refund/ContratException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refund;
+
+/**
+ *
+ * @author Billy
+ */
+class ContratException extends Exception {
+
+    public ContratException() {
+    }
+
+    public ContratException(String msg) {
+        super(msg);
+    }
+}
