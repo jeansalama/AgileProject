114b57970c269bf2260dba3f770ba66f48fcc551
veroniquelucignano <lucignano.veronique@courrier.uqam.ca>
2017-04-19 00:10:16 -0400

    Ajout test@ Classe Entree

--- src/test/java/refund/EntreeTest.java
+++ src/test/java/refund/EntreeTest.java
@@ -14,61 +14,65 @@ import net.sf.json.JSONObject;
 import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
+import static refund.Entree.*;
 
 /**
  *
  * @author Billy
  */
 public class EntreeTest {
-    JSONObject reclamation = new JSONObject();
-   
-        
-       
 
-    
+    JSONObject reclamation = new JSONObject();
+
     @After
     public void tearDown() {
         reclamation = null;
-        
+
     }
-    
+
     @Test
-    public void testObtenirSoinValide() throws ReclamationException{
+    public void testObtenirSoinValide() throws ReclamationException {
         int obtenu;
         reclamation.accumulate("soin", 100);
-         obtenu = Entree.obtenirSoin(reclamation);
-         assertEquals(100, obtenu);
+        obtenu = Entree.obtenirSoin(reclamation);
+        assertEquals(100, obtenu);
     }
-    
+
     @Test(expected = ReclamationException.class)
-    public void testObtenirSoinCléeInvalide() throws ReclamationException{
+    public void testObtenirSoinCléeInvalide() throws ReclamationException {
         int obtenu;
         reclamation.accumulate("soins", 100);
-         obtenu = Entree.obtenirSoin(reclamation);
+        obtenu = Entree.obtenirSoin(reclamation);
     }
-    
+
     @Test(expected = ReclamationException.class)
-    public void testObtenirSoinCléeValeurInvalide() throws ReclamationException{
+    public void testObtenirSoinCléeValeurInvalide() throws ReclamationException {
         int obtenu;
         reclamation.accumulate("soins", "aaaa");
-         obtenu = Entree.obtenirSoin(reclamation);
+        obtenu = Entree.obtenirSoin(reclamation);
     }
-    
+
     @Test(expected = ReclamationException.class)
-    public void testObtenirSoinValeurInvalide() throws ReclamationException{
+    public void testObtenirSoinValeurInvalide() throws ReclamationException {
         int obtenu;
         reclamation.accumulate("soin", "aaaaa");
-         obtenu = Entree.obtenirSoin(reclamation);
+        obtenu = Entree.obtenirSoin(reclamation);
     }
-    
+
     @Test(expected = ReclamationException.class)
-    public void testObtenirSoinCléeValidePasSeule() throws ReclamationException{
+    public void testObtenirSoinCléeValidePasSeule() throws ReclamationException {
         int obtenu;
         reclamation.accumulate("soin aaaaa", 100);
-         obtenu = Entree.obtenirSoin(reclamation);
+        obtenu = Entree.obtenirSoin(reclamation);
     }
 
-}
-    
+    @Test
+    public void testValiderNbrJourAccepteDansReclamation() throws ReclamationException {
+        ArrayList testJourNbrReclamation = new ArrayList(9);
+        testJourNbrReclamation.add("09, 03, 03, 05, 03, 08, 06, 08, 03");
+        validerNbrJourAccepteDansReclamation(testJourNbrReclamation);
+    }
 
+   
+}
