611fd0520d93ff891a31ba0430906e23a187008d
veroniquelucignano <lucignano.veronique@courrier.uqam.ca>
2017-04-18 21:53:14 -0400

    ajout Classe EntreeTest

---
+++ src/test/java/refund/EntreeTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refund;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Billy
+ */
+public class EntreeTest {
+    
+    public EntreeTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
